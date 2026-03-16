5a3d9bbeaa434d0ddff5ea3a2a53b2e81b000710
christopherlangparis-svg <christopher.lang.paris@gmail.com>
2026-03-16 12:16:46 +0100

    Config GitHu Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <-- On importe le moteur Tailwind
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/mon-projet-seo/',
   plugins: [
     react(),
-    tailwindcss(), // <-- On active Tailwind ici
+    tailwindcss(),
   ],
 })
